e347d53749ad7cc229d49ea801d9a583622e0abf
Quincy0v0 <30424046+Quincy0v0@users.noreply.github.com>
2018-09-16 01:06:27 -0500

    rework

--- uss-illini/src/App.js
+++ uss-illini/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Index from './IndexPage/Index.js'
 
 class App extends Component {
   render() {
@@ -8,10 +9,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome Abroad</h1>
         </header>
+          <Index/>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
         </p>
       </div>
     );
